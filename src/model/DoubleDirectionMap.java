6a27061063eae9d2b7aa9941a32cfd643c7237e7
maxime vincent <maxime.vincent1@u-psud.fr>
2020-05-09 12:11:10 +0200

    Add DoubleDirectionMap class

---
+++ src/model/DoubleDirectionMap.java
@@ -0,0 +1,84 @@
+package model;
+import java.util.ArrayList;
+
+/**
+ * @author maxime
+ * @apiNote Dictionnaire pour encoder
+ * et décoder des données
+ * @param <K> Type de la clé
+ * @param <V> Type de la valeur
+ */
+public class DoubleDirectionMap<K, V> {
+	
+	private ArrayList<Pair> map;
+	
+	/**
+	 * @apiNote Crée un dictionnaire vide
+	 */
+	public DoubleDirectionMap() {
+		this.map = new ArrayList<>();
+	}
+	
+	/**
+	 * @apiNote Ajoute Un couple au dictionnaire
+	 * @param key: Clé du couple
+	 * @param value: Valeur du couple
+	 */
+	public void put(K key, V value) {
+		this.map.add(new Pair(key, value));
+	}
+	
+	/**
+	 * @apiNote Encode la clé donnée
+	 * @param key: Clé à encoder
+	 * @return value associée
+	 */
+	public V encode(K key) {
+		for(Pair pair : this.map) {
+			if (key.equals(pair.key)) {
+				return pair.value;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * @apiNote Décode la valeur donnée
+	 * @param value: Valeur à décoder
+	 * @return key associée
+	 */
+	public K decode(V value) {
+		for(Pair pair : this.map) {
+			if(value.equals(pair.value)) {
+				return pair.key;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * @author maxime
+	 * @apiNote Un couple de deux valeurs
+	 * @param <K> Type de la clé
+	 * @param <V> Type de la valeur
+	 */
+	public class Pair {
+		private final K key;
+		private final V value;
+		
+		/**
+		 * @apiNote Crée un couple (clé, valeur) constant
+		 * @param key: Clé du couple
+		 * @param value: Valeur du couple
+		 */
+		public Pair(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return "(" + this.key.toString() + ", " + this.value.toString() + ")";
+		}
+	}
+}
