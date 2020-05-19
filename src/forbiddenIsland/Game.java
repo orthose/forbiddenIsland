11990084d2e9a169731509a199b7b41a11526cb3
Neckfly <baptiste.mqt91@gmail.com>
2020-05-19 13:58:48 +0200

    Création d'une fenêtre graphique

--- src/forbiddenIsland/Game.java
+++ src/forbiddenIsland/Game.java
@@ -3,10 +3,14 @@ import model.IslandModel;
 import view.IslandView;
 
 /**
- * @author maxime
+ * @author maxime & baptiste
  * @apiNote Lance le jeu
  */
 public class Game {
 	public static void main(String[] args) {
+		String map = "a remplir avec une méthode dédié";
+				
+		IslandModel model = new IslandModel(map);
+		IslandView view = new IslandView(model);
 	}
 }
