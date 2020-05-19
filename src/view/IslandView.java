5a6db915d2465c7093f91cbb9c4722f2a4a3bff4
Neckfly <baptiste.mqt91@gmail.com>
2020-05-19 14:00:23 +0200

    Création d'une fenêtre graphique

--- src/view/IslandView.java
+++ src/view/IslandView.java
@@ -1,9 +1,42 @@
 package view;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+import model.IslandModel;
+
 /**
- * @author maxime
+ * @author maxime & baptiste
  * @apiNote Vue de l'île
  */
 public class IslandView {
+	// Contexte graphique
+	private JFrame frame;
 
+	// private VGrid grille;
+	// private VControlleur commandes;
+
+	public IslandView(IslandModel model) {
+		/** Définition de la fenêtre principale. */
+		frame = new JFrame();
+		frame.setTitle("Jeu de la vie de Conway");
+
+		frame.setLayout(new FlowLayout());
+
+		/** Définition des deux vues et ajout à la fenêtre. */
+		// grille = new VueGrille(modele);
+		// frame.add(grille);
+		// commandes = new VueCommandes(modele);
+		// frame.add(commandes);
+
+		/**
+		 * Fin de la plomberie : - Ajustement de la taille de la fenêtre en fonction du
+		 * contenu. - Indiquer qu'on quitte l'application si la fenêtre est fermée. -
+		 * Préciser que la fenêtre doit bien apparaître à l'écran.
+		 */
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
