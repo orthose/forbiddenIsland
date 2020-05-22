ec3c8cae0b177ea3e0c9f84a964aa4d48b9e875e
Neckfly <baptiste.mqt91@gmail.com>
2020-05-22 17:49:49 +0200

    Change test map to add Fire / Earth/ Water

--- src/forbiddenIsland/Game.java
+++ src/forbiddenIsland/Game.java
@@ -7,19 +7,21 @@ import view.IslandView;
  * @apiNote Lance le jeu
  */
 public class Game {
+	public static final long BEGINTIME = System.nanoTime();
+	
 	public static void main(String[] args) {
-		String map = "*********************\n"
-	        	+ "********~~---~*******\n"
-	        	+ "*****~~--------~~****\n"
-	        	+ "***~~~--------~~~****\n"
-	        	+ "**~~~-----------~~~**\n"
-	        	+ "*~~~~~-----------~~~*\n"
-	        	+ "****~~------------~**\n"
-	        	+ "****~~----------~~***\n"
-	        	+ "*******~~---~~*******\n"
-	        	+ "*********************";
+		String map = "***********\n"
+	        	   + "*****--****\n"
+	        	   + "***~-W-****\n"
+	        	   + "**--EE--~**\n"
+	        	   + "*~a-A---~**\n"
+	        	   + "*~~----~***\n"
+	        	   + "**--FF--~**\n"
+	        	   + "**~--F--***\n"
+	        	   + "****~-f****\n"
+	        	   + "***********\n";
 				
 		IslandModel model = new IslandModel(map);
-		IslandView view = new IslandView(model, 500, 500);
+		IslandView view = new IslandView(model, 1000, 1000);
 	}
 }
