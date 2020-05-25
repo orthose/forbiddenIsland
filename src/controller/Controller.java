bd3c4d4d43d980b15225a3a942f6266e4ef5f4ed
Neckfly <baptiste.mqt91@gmail.com>
2020-05-25 19:41:02 +0200

    Change ActionListener to KeyListener + MouseListerner

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,14 +2,21 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
 import model.IslandModel;
 
 /**
- * @author maxime
+ * @author maxime et baptiste
  * @apiNote Le contrôleur chargé de modifier
  * le modèle en fonction des évènements reçus
  */
-public class Controller implements ActionListener {
+public class Controller implements KeyListener, MouseListener/*ActionListener*/ {
 	
 	IslandModel m; // Référence au modèle
 	
@@ -18,12 +25,67 @@ public class Controller implements ActionListener {
 	 */
 	public Controller(IslandModel m) {
 		this.m = m;
+        /*setFocusable(true);
+        setFocusTraversalKeysEnabled(false);*/
+	}
+
+	// Keyboard
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("Maxime a appuyer sur la lettre 'a'");
+		if(e.getKeyChar() == 'a') {
+			System.out.println("Maxime a appuyer sur la lettre 'a'");
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// Mouse
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	/**
 	 * @apiNote Méthode appelée lorsqu'un évènement survient
 	 * @param e: Évènement à traiter
 	 */
-	public void actionPerformed(ActionEvent e) {
-    }
+	/*public void actionPerformed(ActionEvent e) {
+    }*/
 }
