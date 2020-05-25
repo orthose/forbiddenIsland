7b71e6054ab6cff28a5a510d30eb93018fa10750
Neckfly <baptiste.mqt91@gmail.com>
2020-05-25 21:36:24 +0200

    Add movement in Controller class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,18 +3,19 @@ package controller;
 import java.awt.KeyEventDispatcher;
 import java.awt.event.KeyEvent;
 
-
 import model.IslandModel;
+import model.Move;
+import model.Player.InvalidPlayerId;
 
 /**
  * @author maxime et baptiste
- * @apiNote Le contrôleur chargé de modifier
- * le modèle en fonction des évènements reçus
+ * @apiNote Le contrôleur chargé de modifier le modèle en fonction des
+ *          évènements reçus
  */
 public class Controller implements KeyEventDispatcher {
-	
+
 	IslandModel m; // Référence au modèle
-	
+
 	/**
 	 * @param m: Référence au modèle
 	 */
@@ -28,13 +29,22 @@ public class Controller implements KeyEventDispatcher {
 	 * @param e: Évènement à traiter
 	 */
 	public boolean dispatchKeyEvent(KeyEvent e) {
-		 if (e.getID() == KeyEvent.KEY_PRESSED) {
-             System.out.println("tester");
-         } else if (e.getID() == KeyEvent.KEY_RELEASED) {
-             System.out.println("2test2");
-         } else if (e.getID() == KeyEvent.KEY_TYPED) {
-             System.out.println("3test3");
-         }
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+			try {
+				if (e.getKeyCode() == e.VK_UP || e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
+					m.movePlayer(m.getCurrentIdPlayer(), Move.UP);
+				} else if (e.getKeyCode() == e.VK_DOWN || e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
+					m.movePlayer(m.getCurrentIdPlayer(), Move.DOWN);
+				} else if (e.getKeyCode() == e.VK_RIGHT || e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
+					m.movePlayer(m.getCurrentIdPlayer(), Move.RIGHT);
+				} else if (e.getKeyCode() == e.VK_LEFT || e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') {
+					m.movePlayer(m.getCurrentIdPlayer(), Move.LEFT);
+				}
+			} catch (InvalidPlayerId e1) {
+				e1.printStackTrace();
+				System.out.println("Error while moving player in controller");
+			}
+		}
 		return false;
 	}
 
