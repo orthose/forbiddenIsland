67dd693051717ef9b4aba209fb1a568fc15bc37f
Neckfly <baptiste.mqt91@gmail.com>
2020-05-26 19:33:07 +0200

    3 actions points can be use on movement, searching artifact, drying

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -16,7 +16,7 @@ import model.Zone;
 public class Controller implements KeyEventDispatcher {
 
 	private IslandModel m; // Référence au modèle
-	private Action action; // Class interne
+	private int nbAction;
 	private int currentIdPlayer;
 
 	/**
@@ -24,7 +24,7 @@ public class Controller implements KeyEventDispatcher {
 	 */
 	public Controller(IslandModel m) {
 		this.m = m;
-		this.action = new Action();
+		this.nbAction = 3;
 		this.currentIdPlayer = m.nextIdPlayer();
 	}
 
@@ -36,23 +36,23 @@ public class Controller implements KeyEventDispatcher {
 	public boolean dispatchKeyEvent(KeyEvent e) {
 		if (e.getID() == KeyEvent.KEY_PRESSED) {
 			// move
-			if (!action.move) {
+			if (nbAction > 0) {
 				try {
 					if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
 						if (m.movePlayer(currentIdPlayer, Move.UP)) {
-							action.move = true;
+							nbAction--;
 						}
 					} else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
 						if (m.movePlayer(currentIdPlayer, Move.DOWN)) {
-							action.move = true;
+							nbAction--;
 						}
 					} else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
 						if (m.movePlayer(currentIdPlayer, Move.RIGHT)) {
-							action.move = true;
+							nbAction--;
 						}
 					} else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') {
 						if (m.movePlayer(currentIdPlayer, Move.LEFT)) {
-							action.move = true;
+							nbAction--;
 						}
 					}
 				} catch (Exception e1) {
@@ -62,27 +62,27 @@ public class Controller implements KeyEventDispatcher {
 			}
 			
 			// Dry
-			if (!action.dry) {
+			if (nbAction > 0) {
 				try {
 					if (e.getKeyChar() == '5') {
 						if (m.dryPlayer(currentIdPlayer, Move.NONE)) {
-							action.dry = true;
+							nbAction--;
 						}
 					} else if (e.getKeyChar() == '8') {
 						if (m.dryPlayer(currentIdPlayer, Move.UP)) {
-							action.dry = true;
+							nbAction--;
 						}
 					} else if (e.getKeyChar() == '2') {
 						if (m.dryPlayer(currentIdPlayer, Move.DOWN)) {
-							action.dry = true;
+							nbAction--;
 						}
 					} else if (e.getKeyChar() == '6') {
 						if (m.dryPlayer(currentIdPlayer, Move.RIGHT)) {
-							action.dry = true;
+							nbAction--;
 						}
 					} else if (e.getKeyChar() == '4') {
 						if (m.dryPlayer(currentIdPlayer, Move.LEFT)) {
-							action.dry = true;
+							nbAction--;
 						}
 					}
 				} catch (Exception e1) {
@@ -92,7 +92,7 @@ public class Controller implements KeyEventDispatcher {
 			}
 			
 			// Artefact
-			if (!action.artefact) {
+			if (nbAction > 0) {
 				try {
 					if (e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
 						m.findArtefactPlayer(currentIdPlayer);
@@ -105,23 +105,11 @@ public class Controller implements KeyEventDispatcher {
 			
 			// Skip turn
 			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-				action = new Action();
+				nbAction = 3;
 				currentIdPlayer = m.nextIdPlayer();
 			}
 		}
 		return false;
-	}
-
-	public class Action {
-		boolean move;
-		boolean dry;
-		boolean artefact;
-
-		public Action() {
-			this.move = false;
-			this.dry = false;
-			this.artefact = false;
-		}
 	}
 
 }
