777b4d2f91dad4741a7e46faf75684f6f01c5628
Neckfly <baptiste.mqt91@gmail.com>
2020-06-01 11:14:48 +0200

    Add a second view to menu

--- src/forbiddenIsland/Game.java
+++ src/forbiddenIsland/Game.java
@@ -1,65 +1,77 @@
 package forbiddenIsland;
 
 import java.awt.KeyboardFocusManager;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import controller.Controller;
+import controller.MenuController;
 import model.IslandModel;
+import model.Menu;
 import model.Pilot;
 import model.Player;
+import model.Sailor;
 import model.Sexe;
 import util.LevelLoader;
 import util.MusicPlayer;
-import model.Sailor;
 import model.Sexe;
 import view.IslandView;
+import view.MenuView;
 
 /**
  * @author maxime & baptiste
  * @apiNote Lance le jeu
  */
 public class Game extends JFrame {
+	// Clock
 	public static final long BEGINTIME = System.nanoTime();
-	
-	public Game(Controller controller) {
-		  add(new JTextField());
-	      //System.out.println("test");
-	      KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-	      manager.addKeyEventDispatcher(controller);
+	// Command
+	private static ArrayList<String> cmd;
+    // Key manager 
+	private static KeyboardFocusManager manager;
+
+	public Game(MenuController menuController, String[] args) {
+		manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+		manager.addKeyEventDispatcher(menuController);
 	}
 
 	public static void main(String[] args) {
-		
 		// Facilite le traitement de la ligne de commande
-		ArrayList<String> cmd = new ArrayList<String>(args.length);
+		cmd = new ArrayList<String>(args.length);
 		for (int i = 0; i < args.length; i++) {
 			cmd.add(args[i]);
 		}
-		
-		// Charge la carte du niveau spécifié
-		String map = LevelLoader.load(0);
-		
+
+		Menu menu = new Menu();
+		MenuController menuController = new MenuController(menu);
+
+		Game game = new Game(menuController, args);
+
+		MenuView vMenu = new MenuView(menu, menuController);
+	}
+
+	public static void iniGame(IslandModel model) {
 		// Joue la piste audio spécifiée
-		if (! (cmd.contains("-ns") || cmd.contains("--no-sound"))) {
+		if (!(cmd.contains("-ns") || cmd.contains("--no-sound"))) {
 			MusicPlayer.play(0);
 		}
-		
-		IslandModel model = new IslandModel(map);
-		
+
 		// Affichage console
 		if (cmd.contains("-v") || cmd.contains("--verbose")) {
 			model.setVerbose(true);
 		}
-		
-		Player p0 = new Pilot(model, "Pilot", Sexe.MALE);
-		Player p1 = new Sailor(model, "Alexis", Sexe.FEMALE);
-		
-		Controller controller = new Controller(model);	
-		
-		IslandView view = new IslandView(model, controller, 1000, 1000);
-		
-		Game game = new Game(controller);
+
+		Controller controller = new Controller(model);
+
+		IslandView view = new IslandView(model, controller);
+
+		manager.addKeyEventDispatcher(controller);
+	}
+
+	public static KeyboardFocusManager getManager() {
+		return manager;
+
 	}
 }
